ff52585f349efcabcf9183cbdba0f0e220858bb3
12Cy <christian.sandkaemper@gmail.com>
2021-04-18 10:13:14 +0200

    Several settings for presentation have been implemented

---
+++ Assets/Scripts/GUI/ActivateObjectOnKeyInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateObjectOnKeyInput : MonoBehaviour
+{
+	[SerializeField]
+	private List<Element> _objects = new List<Element>();
+
+	private void Update()
+	{
+		foreach (var item in _objects)
+		{
+			if (Input.GetKeyDown(item.KeyCode))
+			{
+				item.ToggleObj();
+			}
+		}
+	}
+
+	[System.Serializable]
+	class Element
+	{
+		public GameObject Object;
+		public KeyCode KeyCode;
+
+		public void ToggleObj()
+		{
+			if (Object.activeInHierarchy)
+			{
+				Object.SetActive(false);
+			}
+			else
+			{
+				Object.SetActive(true);
+			}
+		}
+		
+	}
+}
